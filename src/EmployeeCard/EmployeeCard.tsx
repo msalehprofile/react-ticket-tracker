c1a99f25e154ae0ec4407c2cd8ac98c9d42ab14d
Molly Saleh <Molly@kdvi-macbook-pro-01.home>
2024-04-15 17:09:09 +0100

    added data, started making employee cards

---
+++ src/EmployeeCard/EmployeeCard.tsx
@@ -0,0 +1,19 @@
+import "./EmployeeCard.scss";
+
+
+export type Card = {
+    id: number;
+    name: string;
+    role: string;
+}
+
+const EmployeeCard = () => {
+    if (0)
+    return 
+    <div>
+        <p>name</p>
+        <p>role</p>
+    </div>
+};
+
+export default EmployeeCard;
